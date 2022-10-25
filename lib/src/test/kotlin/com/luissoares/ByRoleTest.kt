ce4cd4f8dcd17f34a1591ccdc4056fcbf1896507
Luís Soares <lsoares@gmail.com>
2022-10-25 23:13:22 +0100

    join examples

--- lib/src/test/kotlin/com/luissoares/ByRoleTest.kt
+++ lib/src/test/kotlin/com/luissoares/ByRoleTest.kt
@@ -2,7 +2,8 @@ package com.luissoares
 
 import org.junit.jupiter.api.extension.ExtendWith
 import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.ValueSource
+import org.junit.jupiter.params.provider.Arguments.of
+import org.junit.jupiter.params.provider.MethodSource
 import org.openqa.selenium.remote.RemoteWebDriver
 import kotlin.test.assertEquals
 
@@ -10,122 +11,71 @@ import kotlin.test.assertEquals
 class ByRoleTest(private val driver: RemoteWebDriver) {
 
     @ParameterizedTest
-    @ValueSource(
-        strings = [
-            """<div role="textbox"
+    @MethodSource("examples")
+    fun `by role`(role: String, content: String) {
+        driver.getFromHtml(content)
+
+        val result = driver.findElement(ByRole(role))
+
+        assertEquals(role, result.ariaRole)
+    }
+
+    private fun examples() = setOf(
+        of(
+            "textbox", """<div role="textbox"
                     contenteditable="true"
                     aria-placeholder="5-digit zipcode"
                     aria-labelledby="txtboxLabel">
-               </div>""",
-            """<input type="text" placeholder="5-digit zipcode" id="txtbox" />""",
-            """<textarea id="txtboxMultiline" required></textarea>"""
-        ]
-    )
-    fun textbox(content: String) {
-        driver.getFromHtml(content)
-
-        val result = driver.findElement(ByRole("textbox"))
-
-        assertEquals("textbox", result.ariaRole)
-    }
-
-    @ParameterizedTest
-    @ValueSource(
-        strings = [
-            """<h1>h1</h1>""",
-            """<h1>h2</h1>""",
-            """<h1>h3</h1>""",
-            """<h1>h4</h1>""",
-            """<h1>h5</h1>""",
-            """<h1>h6</h1>""",
-            """<div role="heading" aria-level="1">This is a main page heading</div>""",
-        ]
-    )
-    fun heading(content: String) {
-        driver.getFromHtml(content)
-
-        val result = driver.findElement(ByRole("heading"))
-
-        assertEquals("heading", result.ariaRole)
-    }
-
-    @ParameterizedTest
-    @ValueSource(
-        strings = [
-            """<div id="saveChanges" tabindex="0" role="button" aria-pressed="false">Save</div>""",
-            """<button type="button" id="saveChanges">Save</button>""",
-        ]
-    )
-    fun button(content: String) {
-        driver.getFromHtml(content)
-
-        val result = driver.findElement(ByRole("button"))
-
-        assertEquals("button", result.ariaRole)
-    }
-
-    @ParameterizedTest
-    @ValueSource(
-        strings = [
-            """<div role="article">
+               </div>"""
+        ),
+        of("textbox", """<input type="text" placeholder="5-digit zipcode" id="txtbox" />"""),
+        of("textbox", """<textarea id="txtboxMultiline" required></textarea>"""),
+        of("heading", """<h1>h1</h1>"""),
+        of("heading", """<h1>h2</h1>"""),
+        of("heading", """<h1>h3</h1>"""),
+        of("heading", """<h1>h4</h1>"""),
+        of("heading", """<h1>h5</h1>"""),
+        of("heading", """<h1>h6</h1>"""),
+        of("heading", """<div role="heading" aria-level="1">This is a main page heading</div>"""),
+        of("button", """"<div id="saveChanges" tabindex="0" role="button" aria-pressed="false">Save</div>"""),
+        of("button", """<button type="button" id="saveChanges">Save</button>"""),
+        of(
+            "article", """<div role="article">
                   <h2>Heading of the segment</h2>
                   <p>Paragraph for the segment.</p>
                   Controls to interact with the article, share it, etc.
-               </div>""",
-            """<article>
+               </div>"""
+        ),
+        of(
+            "article", """<article>
                   <h2>Heading of the segment</h2>
                   <p>Paragraph for the segment.</p>
                   Controls to interact with the article, share it, etc.
                 </article>
-            """,
-        ]
-    )
-    fun article(content: String) {
-        driver.getFromHtml(content)
-
-        val result = driver.findElement(ByRole("article"))
-
-        assertEquals("article", result.ariaRole)
-    }
-
-    @ParameterizedTest
-    @ValueSource(
-        strings = [
-            """<div role="banner">
+            """
+        ),
+        of(
+            "banner", """<div role="banner">
                   <h2>Heading of the segment</h2>
                   Controls to interact with the article, share it, etc.
-               </div>""",
-            """<header>
+               </div>"""
+        ),
+        of(
+            "banner", """<header>
                   <a href="#main" id="skipToMain" class="skiptocontent">Skip To main content</a>
                   <img src="images/w3c.png" alt="W3C Logo" />
-               </header>""",
-        ]
-    )
-    fun banner(content: String) {
-        driver.getFromHtml(content)
-
-        val result = driver.findElement(ByRole("banner"))
-
-        assertEquals("banner", result.ariaRole)
-    }
-
-    @ParameterizedTest
-    @ValueSource(
-        strings = [
-            """<<span role="checkbox"
+               </header>"""
+        ),
+        of(
+            "checkbox", """<<span role="checkbox"
                       aria-checked="false"
                       tabindex="0"
                       aria-labelledby="chk1-label"></span>
-                    <label id="chk1-label">Remember my preferences</label>""",
-            """<input type="checkbox" id="chk1-label" />
-               <label for="chk1-label">Remember my preferences</label>""",
-        ]
+                    <label id="chk1-label">Remember my preferences</label>"""
+        ),
+        of(
+            "checkbox", """<input type="checkbox" id="chk1-label" />
+               <label for="chk1-label">Remember my preferences</label>"""
+        ),
     )
-    fun checkbox(content: String) {
-        driver.getFromHtml(content)
-
-        val result = driver.findElement(ByRole("checkbox"))
-
-        assertEquals("checkbox", result.ariaRole)
-    }
 }
