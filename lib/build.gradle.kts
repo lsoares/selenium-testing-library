8dec0e9c6ab620cf112c49ac2f3904c20ebee727
Luís Soares <lsoares@gmail.com>
2022-10-25 23:13:21 +0100

    simplify gradle config

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -10,10 +10,9 @@ repositories {
 }
 
 dependencies {
-    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
     implementation("org.seleniumhq.selenium:selenium-java:4.5.0")
-    api("com.github.sam-rosenthal:java-cssSelector-to-xpath:V1.0.0RC1")
+    implementation("com.github.sam-rosenthal:java-cssSelector-to-xpath:V1.0.0RC1")
     testImplementation(kotlin("test"))
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.1")
     testImplementation("io.github.bonigarcia:webdrivermanager:5.3.0")
