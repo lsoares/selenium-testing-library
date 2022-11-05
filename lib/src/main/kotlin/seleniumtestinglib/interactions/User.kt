d18c58c053be1c70997a23d4cac43fc44401b68f
Luís Soares <lsoares@gmail.com>
2022-11-05 14:27:33 +0000

    validate options

--- lib/src/main/kotlin/seleniumtestinglib/interactions/User.kt
+++ lib/src/main/kotlin/seleniumtestinglib/interactions/User.kt
@@ -7,18 +7,37 @@ import seleniumtestinglib.ensureScript
 class User(val javascriptExecutor: JavascriptExecutor)
 
 // TODO: test receiving options
-// TODO: limit valid options
+/**
+ * Obtains a custom instance of user by calling `userEvent.setup()` with the supplied arguments.
+ * https://testing-library.com/docs/user-event/options
+ */
 fun RemoteWebDriver.user(vararg options: Pair<String, Any?>): User {
+    require(options.all { it.first in validUserOptions })
     ensureScript("user-event.js", "window.userEvent?.setup")
     executeScript("user = userEvent.setup(arguments[0])", options.toMap())
     return User(this as JavascriptExecutor)
 }
 
+/**
+ * If there's one Testing Library `user` already, it returns it.
+ * Else, it creates a default one (obtained with `userEvent.setup()`).
+ */
 val RemoteWebDriver.user: User
-    get() {
-        val thereIsUser = executeScript("return typeof user != 'undefined'") as Boolean
-        return when {
-            thereIsUser -> User(this as JavascriptExecutor)
-            else        -> user()
-        }
-    }
+    get() = if (executeScript("return typeof user != 'undefined'") as Boolean)
+        User(this as JavascriptExecutor)
+    else user()
+
+private val validUserOptions = setOf(
+    "advanceTimers",
+    "applyAccept",
+    "autoModify",
+    "delay",
+    "document",
+    "keyboardMap",
+    "pointerEventsCheck",
+    "pointerMap",
+    "skipAutoClose",
+    "skipClick",
+    "skipHover",
+    "writeToClipboard",
+)
