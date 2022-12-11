37eff9ad00ae4e125f642d9d71431993fc337f60
Luís Soares <lsoares@gmail.com>
2022-12-11 17:50:01 +0000

    deal with nulls

--- lib/src/main/kotlin/seleniumtestinglib/jestdom/Api.kt
+++ lib/src/main/kotlin/seleniumtestinglib/jestdom/Api.kt
@@ -9,7 +9,6 @@ import seleniumtestinglib.*
  */
 fun expect(element: WebElement?) = JestDomMatcher(element)
 
-// TODO: test nulls
 data class JestDomMatcher(
     private val element: WebElement?,
     private val requireTrue: Boolean = true,
@@ -18,20 +17,24 @@ data class JestDomMatcher(
     val not get() = JestDomMatcher(element, requireTrue.not())
 
     fun toBeDisabled() {
-        validate(element?.isDisabled == true)
+        requireNotNull(element)
+        validate(element.isDisabled)
     }
 
     fun toBeEnabled() {
-        validate(element?.isEnabled == true)
+        requireNotNull(element)
+        validate(element.isEnabled)
     }
 
     fun toBeEmptyDomElement() {
-        val innerHtml = element?.getAttribute("innerHTML")?.replace("<!--.*?-->".toRegex(), "")
-        validate(innerHtml?.isEmpty() == true)
+        requireNotNull(element)
+        val innerHtml = element.getAttribute("innerHTML").replace("<!--.*?-->".toRegex(), "")
+        validate(innerHtml.isEmpty())
     }
 
     fun toBeInvalid() {
-        validate(element?.isInvalid == true)
+        requireNotNull(element)
+        validate(element.isInvalid)
     }
 
     fun toBeInTheDocument() {
@@ -39,83 +42,98 @@ data class JestDomMatcher(
     }
 
     fun toBeRequired() {
-        validate(element?.isRequired == true)
+        requireNotNull(element)
+        validate(element.isRequired)
     }
 
     fun toBeValid() {
-        validate(element?.isValid == true)
+        requireNotNull(element)
+        validate(element.isValid)
     }
 
     fun toBeVisible() {
-        validate(element?.isVisible == true)
+        requireNotNull(element)
+        validate(element.isVisible)
     }
 
     fun toContainElement(ancestor: WebElement?) {
-        validate(element?.findElements(By.xpath(".//*"))?.contains(ancestor) == true)
+        requireNotNull(element)
+        validate(element.findElements(By.xpath(".//*")).contains(ancestor))
     }
 
     fun toContainHtml(htmlText: String) {
-        val normalizedHtmlText = element?.wrappedDriver?.executeScript(
+        requireNotNull(element)
+        val normalizedHtmlText = element.wrappedDriver.executeScript(
             """
             const parser = new DOMParser()
             const htmlDoc = parser.parseFromString(arguments[0], "text/html")
             return htmlDoc.querySelector("body").innerHTML
         """, htmlText
-        ) as? String
-        validate(element?.innerHtml?.contains(normalizedHtmlText.orEmpty()) == true)
+        ) as String
+        validate(element.innerHtml.contains(normalizedHtmlText))
     }
 
     fun toHaveAccessibleDescription() {
-        validate(element?.accessibleDescription?.isNotBlank() == true)
+        requireNotNull(element)
+        validate(element.accessibleDescription.isNotBlank())
     }
 
     fun toHaveAccessibleDescription(expectedAccessibleDescription: String) {
-        compare(expectedAccessibleDescription, element?.accessibleDescription)
+        requireNotNull(element)
+        compare(expectedAccessibleDescription, element.accessibleDescription)
     }
 
     fun toHaveAccessibleDescription(expectedAccessibleDescription: Regex) {
-        validate(expectedAccessibleDescription.find(element?.accessibleDescription.orEmpty()) != null)
+        requireNotNull(element)
+        validate(expectedAccessibleDescription.find(element.accessibleDescription) != null)
     }
 
     fun toHaveAccessibleName() {
-        validate(element?.accessibleName?.isNotBlank() == true)
+        requireNotNull(element)
+        validate(element.accessibleName.isNotBlank())
     }
 
     fun toHaveAccessibleName(expectedAccessibleName: String) {
-        compare(expectedAccessibleName, element?.accessibleName)
+        requireNotNull(element)
+        compare(expectedAccessibleName, element.accessibleName)
     }
 
     fun toHaveAccessibleName(expectedAccessibleName: Regex) {
-        validate(expectedAccessibleName.find(element?.accessibleDescription.orEmpty()) != null)
+        requireNotNull(element)
+        validate(expectedAccessibleName.find(element.accessibleDescription) != null)
     }
 
     fun toHaveAttribute(attribute: String) {
-        validate(element?.getAttribute(attribute)?.isNotBlank() == true)
+        requireNotNull(element)
+        validate(element.getAttribute(attribute)?.isNotBlank() == true)
     }
 
     fun toHaveAttribute(attribute: String, value: String) {
-        compare(value, element?.getAttribute(attribute))
+        requireNotNull(element)
+        compare(value, element.getAttribute(attribute))
     }
 
     fun toHaveClass(vararg classNames: String, exact: Boolean = false) {
+        requireNotNull(element)
         val expectedClasses = classNames.map { it.split(Regex("\\s+")) }.flatten().toSet()
-        val elementClasses = element?.classes ?: emptySet()
         if (expectedClasses.isEmpty()) {
-            validate(elementClasses.isNotEmpty())
+            validate(element.classes.isNotEmpty())
             return
         }
         when (exact) {
-            false -> validate(elementClasses.containsAll(expectedClasses), mapOf("elementClasses" to elementClasses))
-            true  -> compare(expectedClasses, elementClasses)
+            false -> validate(element.classes.containsAll(expectedClasses), mapOf("elementClasses" to element.classes))
+            true  -> compare(expectedClasses, element.classes)
         }
     }
 
     fun toHaveFocus() {
-        validate(element?.isFocused == true)
+        requireNotNull(element)
+        validate(element.isFocused)
     }
 
     fun toHaveFormValues(vararg values: Pair<String, Any?>) {
-        val formValues = element?.formValues ?: emptyMap()
+        requireNotNull(element)
+        val formValues = element.formValues
         validate(
             values.toMap().all { formValues[it.key] == it.value },
             mapOf("expected values" to values.toMap(), "form values" to formValues)
@@ -123,80 +141,95 @@ data class JestDomMatcher(
     }
 
     fun toHaveStyle(css: String) {
+        requireNotNull(element)
         val expectedCss = css.split(";")
             .filter(String::isNotBlank)
             .associate {
                 it.split(":").let { it.first().trim().normalizeCssProp() to it.last().trim() }
             }
         val existingCss = expectedCss.keys
-            .associateWith { element?.getCssValue(it) }
+            .associateWith { element.getCssValue(it) }
         compare(expectedCss, existingCss)
     }
 
     fun toHaveStyle(css: Map<String, String>) {
+        requireNotNull(element)
         val expectedCss = css.mapKeys { it.key.normalizeCssProp() }
         val existingCss = expectedCss.keys
-            .associateWith { element?.getCssValue(it) }
+            .associateWith { element.getCssValue(it) }
         compare(expectedCss, existingCss)
     }
 
     private fun String.normalizeCssProp() = "(?<=[a-zA-Z])[A-Z]".toRegex().replace(this) { "-${it.value}" }.lowercase()
 
     fun toHaveTextContent(text: String) {
-        validate(element?.text?.contains(text) == true, mapOf("text" to element?.text))
+        requireNotNull(element)
+        validate(element.text?.contains(text) == true, mapOf("text" to element.text))
     }
 
     fun toHaveTextContent(text: Regex) {
-        validate(element?.text?.let { text.find(it) } != null, mapOf("text" to element?.text))
+        requireNotNull(element)
+        validate(element.text?.let { text.find(it) } != null, mapOf("text" to element.text))
     }
 
     fun toHaveValue() {
-        compare(element?.value, null)
+        requireNotNull(element)
+        compare(element.value, null)
     }
 
     fun toHaveValue(value: String) {
-        compare(element?.value, value)
+        requireNotNull(element)
+        compare(element.value, value)
     }
 
     fun toHaveValue(value: Int) {
-        compare(element?.value, value)
+        requireNotNull(element)
+        compare(element.value, value)
     }
 
     fun toHaveValue(value: List<String>) {
-        compare(element?.value, value)
+        requireNotNull(element)
+        compare(element.value, value)
     }
 
     fun toHaveDisplayValue(value: String) {
-        compare(value, element?.displayValue)
+        requireNotNull(element)
+        compare(value, element.displayValue)
     }
 
     fun toHaveDisplayValue(value: Regex) {
-        validate(element?.displayValue?.let {
+        requireNotNull(element)
+        validate(element.displayValue?.let {
             if (it is List<*>) it.filter { value.find(it.toString()) != null }.takeIf(List<Any?>::isNotEmpty)
             else value.find(it.toString())
-        } != null, mapOf("text" to element?.text))
+        } != null, mapOf("text" to element.text))
     }
 
     fun toHaveDisplayValue(value: List<String>) {
-        compare(value, element?.displayValue)
+        requireNotNull(element)
+        compare(value, element.displayValue)
     }
 
     fun toBeChecked() {
-        validate(element?.isChecked == true)
+        requireNotNull(element)
+        validate(element.isChecked)
     }
 
     fun toBePartiallyChecked() {
-        validate(element?.isPartiallyChecked == true)
+        requireNotNull(element)
+        validate(element.isPartiallyChecked)
     }
 
     fun toHaveErrorMessage(text: String) {
-        compare(text, element?.errorMessage)
+        requireNotNull(element)
+        compare(text, element.errorMessage)
     }
 
     fun toHaveErrorMessage(text: Regex) {
+        requireNotNull(element)
         validate(
-            text.find(element?.errorMessage.orEmpty()) != null,
-            mapOf("text" to text, "error message" to element?.errorMessage)
+            text.find(element.errorMessage.orEmpty()) != null,
+            mapOf("text" to text, "error message" to element.errorMessage)
         )
     }
 
